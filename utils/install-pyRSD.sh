301b690159af6d80a861b13ced382ecfba4e0b9a
Nick Hand <nhand@berkeley.edu>
2018-02-17 14:58:04 -0500

    make sure correct programming environment is loaded on NERSC

--- utils/install-pyRSD.sh
+++ utils/install-pyRSD.sh
@@ -12,5 +12,12 @@ cd $build_dir
 module unload fftw || true
 module load fftw/3.3.4.6
 
+# load gnu environment
+module unload PrgEnv-intel || true
+module load PrgEnv-gnu
+
+# load swig
+module load swig
+
 # tar pyRSD
 pip install -vvv -I --no-deps --global-option=build_ext --global-option="-I$FFTW_INC" pyRSD
